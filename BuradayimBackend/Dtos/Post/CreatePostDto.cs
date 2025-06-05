c79929275877c79aa9cac5302babf0e15c482666
Aziz Kızgın <azizkzgn@gmail.com>
2025-06-05 10:41:15 +0300

    feat: make UserId nullable in CreatePostDto

--- BuradayimBackend/Dtos/Post/CreatePostDto.cs
+++ BuradayimBackend/Dtos/Post/CreatePostDto.cs
@@ -10,7 +10,7 @@ namespace BuradayimBackend.Dtos.Post
         public string Title { get; set; }
         public string Content { get; set; }
         public List<string> Images { get; set; } = new();
-        public string UserId { get; set; }
+        public string? UserId { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
     }
